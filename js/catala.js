cd8a2daf8e37611303d7d472f7125c9c3e47f604
Shmarte1970 <shmarte@gmail.com>
2024-04-12 10:49:22 +0200

    update 2024

--- js/catala.js
+++ js/catala.js
@@ -1,72 +1,204 @@
-document.getElementById('catala').onclick = function(){
-  document.getElementById('m1').innerHTML ="Qui Sóc";
-  document.getElementById('m2').innerHTML ="Experiència";
-  document.getElementById('m3').innerHTML ="formació";
-  document.getElementById('m4').innerHTML ="Skill";
-  document.getElementById('m5').innerHTML ="Competencia";
-  document.getElementById('m6').innerHTML ="Contacte";  
-  document.getElementById('catala').innerHTML ="CATALÀ - ";
-  document.getElementById('castellano').innerHTML ="CASTELLÀ";
+document.getElementById("catala").onclick = function () {
+  document.getElementById("m1").innerHTML = "Qui Sóc";
+  document.getElementById("m2").innerHTML = "Experiència";
+  document.getElementById("m3").innerHTML = "formació";
+  document.getElementById("m4").innerHTML = "Skill";
+  document.getElementById("m5").innerHTML = "Competencia";
+  document.getElementById("m6").innerHTML = "Contacte";
+  document.getElementById("catala").innerHTML = "CATALÀ - ";
+  document.getElementById("castellano").innerHTML = "CASTELLÀ";
   // document.getElementById('english').innerHTML='ENGLISH';/
-  document.getElementById('f1').innerHTML ="Experiència en la direcció d'equips Help-Desk, Operadors i Atenció al client. Tinc una gran capacitat per enfocar i organitzar"+"<br>"+" grups mantenint un bon ambient de treball."+"<br>"+"<br>"+"Facilitat per desenvolupar operatives eficients."+"<br>"+"<br>"+"Treballs de programació freelancer en diferents Llenguatges. Des de la planificació fins a la finalització del Projecte."+"<br>"+"<br>"+"Actualment estic interessat a formar part d'un equip de treball en el Desenvolupament d'Aplicacions web o Java. Són àrees professionals que em fascinen i en què em trobo molt còmode. També m'agradaria explorar altres àrees com el SEO que ja estudi o els comerços virtuals, entre d'altres tecnologies ."+"<br>"+"<br>"+"Em considero una persona extrovertida i proactiva que sempre busca aconseguir els seus objectius.";
-  document.getElementById('m7').innerHTML ="Experiència";
-  document.getElementById('exp-tl-1').innerHTML ="Suport i Desenvolupament";
-  document.getElementById('exp-tl-62').innerHTML ="Tècnic Informàtic";
-  document.getElementById('exp-tl-61').innerHTML ="Sagrada Familia Tècnic Informàtic";
-  document.getElementById('exp-tl-80').innerHTML =" Desenvolupador ERP FULL-STACK";
-  document.getElementById('exp-tl-70').innerHTML ="Desenvolupador ERP AXIONAL";
-  
-  document.getElementById('exp-tl-50').innerHTML ="Tècnic Informàtic";
+  document.getElementById("f1").innerHTML =
+    "<br>" +
+    "Amb una sòlida experiència en direcció d'equips de programació i helpdesk, així com en atenció al client" +
+    "<br>" +
+    "<br>" +
+    " Em destaco per la meva capacitat per treballar en equip i la meva habilitat en la programació FullStack i bases de dades SQL." +
+    "<br>" +
+    "<br>" +
+    "Sóc una persona proactiva, extrovertida i assertiva, sempre a la recerca de la millor solució i assolir els objectius establerts." +
+    "<br>" +
+    "<br>" +
+    "El meu enfocament centrat en l'equip i la meva determinació per superar desafiaments em permeten destacar en entorns dinàmics i exigents.reballs de programació freelancer en diferents Llenguatges. Des de la planificació fins a la finalització del Projecte." +
+    "<br>" +
+    "<br>";
+  document.getElementById("m7").innerHTML = "Experiència";
+  document.getElementById("exp-tl-1").innerHTML = "Suport i Desenvolupament";
+  document.getElementById("exp-tl-62").innerHTML = "Tècnic Informàtic";
+  document.getElementById("exp-tl-61").innerHTML =
+    "Sagrada Familia Tècnic Informàtic";
+  document.getElementById("exp-tl-80").innerHTML =
+    " Desenvolupador ERP FULL-STACK";
+  document.getElementById("exp-tl-70").innerHTML = "Desenvolupador ERP AXIONAL";
 
-  document.getElementById('exp-leyenda-80').innerHTML ="Desenvolupament ERP pròpia de l'Empresa"+"<br>"+"Desenvolupament Querys SQL"+"<br>"+"Desenvolupament del Projecte ERP"+"<br>"+"Desenvolupament Full Stack (React Node-Express)"+"<br>"+"Administració AWS";
+  document.getElementById("exp-tl-50").innerHTML = "Tècnic Informàtic";
 
-  document.getElementById('exp-leyenda-70').innerHTML ="Resolució d'incidències usuaris ERP Axional"+"<br>"+"Desenvolupament de Noves opcions a l'ERP amb XSQL"+"<br>"+"Desenvolupament de Querys XSQL - SQL"+"<br>"+"Resolució Bugs a ERP"+"<br>"+"Gestió d'incidències amb GIRA";
+  document.getElementById("exp-leyenda-80").innerHTML =
+    "Anàlisi per al desenvolupament del propi ERP de l'empresa" +
+    "<br>" +
+    "Desenvolupament de taules i consultes SQL" +
+    "<br>" +
+    "Desenvolupament Full Stack (Angular Node-Express)" +
+    "<br>" +
+    "Publicació i administració a AWS";
 
-  document.getElementById('exp-leyenda-62').innerHTML ="Maquetació d'Equips, Configuració VPN (Teletreball)"+"<br>"+"HelpDesk, Configuració d'impresores i Scanners, OneDrive, Outlook"+"<br>"+"Active Directory."+"<br>"+"Seguiment incidències con Software Remdy";
+  document.getElementById("exp-leyenda-70").innerHTML =
+    "Resolució d'incidències usuaris ERP Axional" +
+    "<br>" +
+    "Desenvolupament de Noves opcions a l'ERP amb XSQL" +
+    "<br>" +
+    "Desenvolupament de Querys XSQL - SQL" +
+    "<br>" +
+    "Resolució Bugs a ERP" +
+    "<br>" +
+    "Gestió d'incidències amb GIRA";
 
-  document.getElementById('exp-leyenda-54').innerHTML ="Consultes Oracle (SQL)"+"<br>"+"Ofimàtica, Instal·lació i manteniment de PC'S i Xarxes"+"<br>"+"Disseny i Manteniment web"+"<br>"+"Atenció a l'Usuari."+"<br>"+"Instalacionse Teletreball i Atenció a l'Usuari Teletreball."+"<br>"+"Eines de disseny i Fotografia Digital.";
+  document.getElementById("exp-leyenda-62").innerHTML =
+    "Maquetació d'Equips, Configuració VPN (Teletreball)" +
+    "<br>" +
+    "HelpDesk, Configuració d'impresores i Scanners, OneDrive, Outlook" +
+    "<br>" +
+    "Active Directory." +
+    "<br>" +
+    "Seguiment incidències con Software Remdy";
 
-  document.getElementById('exp-leyenda-61').innerHTML ="Configuració & Maquetació d'Equips."+"<br>"+"Configuració de Treball Remot Forticlient"+"<br>"+"Reubicació d'usuaris, Actualització de llicències"+"<br>"+"Mantenimineto de Impresoras."+"<br>"+"Desenvolupament App. HTML, CSS, JS"+"<br>"+"Eines Gestion Tickets";
+  document.getElementById("exp-leyenda-54").innerHTML =
+    "Consultes Oracle (SQL)" +
+    "<br>" +
+    "Ofimàtica, Instal·lació i manteniment de PC'S i Xarxes" +
+    "<br>" +
+    "Disseny i Manteniment web" +
+    "<br>" +
+    "Atenció a l'Usuari." +
+    "<br>" +
+    "Instalacionse Teletreball i Atenció a l'Usuari Teletreball." +
+    "<br>" +
+    "Eines de disseny i Fotografia Digital.";
 
-  document.getElementById('exp-data-55').innerHTML ="2022-2023";
-  document.getElementById('exp-data-1').innerHTML = "Febrer/2014 - Maig/2016"
-  document.getElementById('exp-leyenda-1').innerHTML ="Suport a usuaris d'Oficina i mans remotes."+"<br>"+"Maquetación d'Equips Informàtics"+"<br>"+"Col·laboració en diferents projectes de programació."+"<br>"+"Llenguatges Java, HTML, CSS, JS, PHP, MySQL.";
-  document.getElementById('exp-tl-2').innerHTML ="COORDINADOR OPERACIONS IBM";
-  document.getElementById('exp-data-2').innerHTML ="Maig/2010 - Febrer/2014";
-  document.getElementById('exp-leyenda-2').innerHTML ="Coordinador Equip Operadors 24x7."+"<br>"+"Implementació d'horaris i torns de treball."+"<br>"+"Monitorització Xarxes amb Nagios"+"<br>"+"Desenvolupament i Implementació de procediments habituals de treball."+"<br>"+"Disseny d'aplicacions per al control estocs cintes clients."+"<br>"+"Supervisió Gestion i Manteniment de l'estoc de servidors instal·lats al CPD."+"<br>"+"Control Accés Personal CPD.";
-  document.getElementById('exp-tl-3').innerHTML ="RESPONSABLE HELP-DESK I MICROINFORMÀTICA";
-  document.getElementById('exp-data-3').innerHTML ="Abril/2006 - Setembre/2007";
-  document.getElementById('exp-leyenda-3').innerHTML ="Responsable de l'Help-Desk, Usuaris Locals i Clients en Remot."+"<br>"+"Compres, Integració i configuració Equips Informàtics."+"<br>"+"Gestió d'Incidències entre Clients finals i el dpt. de Desenvolupament."+"<br>"+"Documentació del Software desenvolupat.";
-  document.getElementById('exp-tl-4').innerHTML ="Desenvolupament Aplicacions WEB Front-end Back-end";
-  document.getElementById('exp-data-4').innerHTML ="2019";
-  document.getElementById('exp-tl-5').innerHTML ="Desenvolupament Java, Script Bash Linux";
-  document.getElementById('exp-leyenda-4').innerHTML ="Retoc Gràfic: Photoshop."+"<br>"+"Disseny Web: Bootstrap, WordPress."+"<br>"+"Llenguatges de programació: HMTL5, CSS3, JavaScript, Jquery, PHP, SQL, XML, Java, Android, Mit App Inventor 2."+"<br>"+"Estándares de programación WEB: Estricto seguimiento de las recomendaciones de w3schools."+"<br>"+"Atenció Personalitzada a clients."+"<br>"+"Hosting & Posicionament SEO";
-  document.getElementById('exp-leyenda-5').innerHTML ="Desenvolupament i mantenimineto dels Projectes Java existents"+"<br>"+"Llenguatges de programació: Java, Script Bash"+"<br>"+"Estàndards: Ide Eclipse, SVN, Git";
-  document.getElementById('exp-data-5').innerHTML ="2018";
-  document.getElementById('estudios-1').innerHTML ="Grau Superior Desenvolupament Aplicacions WEB";
-  document.getElementById('estudios-52').innerHTML ="Desenvolupament Aplicacions WEB amb Java, JSP, Servlet, Spring, Struts, Hibernate";
-  document.getElementById('centro-1').innerHTML ="Any realització 2017-2018 ";
-  document.getElementById('formacion').innerHTML ="formació";
-  document.getElementById('estudios-2').innerHTML ="Desenvolupament d´aplicacions JAVA";
-  document.getElementById('centro-2').innerHTML ="Any realització 2018";
-  document.getElementById('centro-7').innerHTML ="Any realització 2017";
-  document.getElementById('estudios-3').innerHTML ="Desenvolupament JAVA, Programació Mòbils Avançada XML";
-  document.getElementById('estudios-4').innerHTML ="FP-II Informàtica de Gestió ";
-  document.getElementById('estudios-5').innerHTML ="Formació continuada Autodidacta desenvolupament";
-  document.getElementById('estudios-20').innerHTML ="Curs Social Media Market Orientat a l'Comerç i Xarxes Socials";
-  document.getElementById('estudios-51').innerHTML ="Mit App Inventor 2"+"<br>"+"Certificat SoloLearn SQL Fundaments course"+"<br>"+"posicionament SEO"+"<br>"+"Angular"+"<br>"+"Python"+"<br>"+"Sass"+"<br>"+"SQL con Oracle 11g"+"<br>"+"C y C++";
-  document.getElementById('estudios-6').innerHTML ="Curs de Desemvolupament Back-end amb PHP";
-  document.getElementById('centro-6').innerHTML ="Any realització 2019";
-  document.getElementById('centro-16').innerHTML ="Any realització 2019";
-  document.getElementById('estudios-71').innerHTML ="Curs SEO-SEM Posicionament WEB";
-  document.getElementById('centro-3').innerHTML ="Any realització 2014-2016 ";
-  document.getElementById('centro-4').innerHTML ="Any realització 1990 ";
-  document.getElementById('centro-8').innerHTML ="Any realització 2020";
-  document.getElementById('Interes-Lot').innerHTML ="COMPETÈNCIA";
-  document.getElementById('compete').src="img/competencias-cat.jpg";
-  document.getElementById('activa').innerHTML ="&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspInformació obtinguda a través dels serveis de competència de";
+  document.getElementById("exp-leyenda-61").innerHTML =
+    "Configuració & Maquetació d'Equips." +
+    "<br>" +
+    "Configuració de Treball Remot Forticlient" +
+    "<br>" +
+    "Reubicació d'usuaris, Actualització de llicències" +
+    "<br>" +
+    "Mantenimineto de Impresoras." +
+    "<br>" +
+    "Desenvolupament App. HTML, CSS, JS" +
+    "<br>" +
+    "Eines Gestion Tickets";
 
- /*    document.getElementById('Interes-l1').innerHTML ="Sóc una persona resolutiva. Capaç de portar la iniciativa en els treballs d'equip i en situacions difícils o estrès. Tinc disposició participar i aportar idees. Sóc capaç de preveure problemes i anticipar-me amb la solució.";
+  document.getElementById("exp-data-55").innerHTML = "2022-2023";
+  document.getElementById("exp-data-1").innerHTML = "Febrer/2014 - Maig/2016";
+  document.getElementById("exp-leyenda-1").innerHTML =
+    "Suport a usuaris d'Oficina i mans remotes." +
+    "<br>" +
+    "Maquetación d'Equips Informàtics" +
+    "<br>" +
+    "Col·laboració en diferents projectes de programació." +
+    "<br>" +
+    "Llenguatges Java, HTML, CSS, JS, PHP, MySQL.";
+  document.getElementById("exp-tl-2").innerHTML = "COORDINADOR OPERACIONS IBM";
+  document.getElementById("exp-data-2").innerHTML = "Maig/2010 - Febrer/2014";
+  document.getElementById("exp-leyenda-2").innerHTML =
+    "Coordinador Equip Operadors 24x7." +
+    "<br>" +
+    "Implementació d'horaris i torns de treball." +
+    "<br>" +
+    "Monitorització Xarxes amb Nagios" +
+    "<br>" +
+    "Desenvolupament i Implementació de procediments habituals de treball." +
+    "<br>" +
+    "Disseny d'aplicacions per al control estocs cintes clients." +
+    "<br>" +
+    "Supervisió Gestion i Manteniment de l'estoc de servidors instal·lats al CPD." +
+    "<br>" +
+    "Control Accés Personal CPD.";
+  document.getElementById("exp-tl-3").innerHTML =
+    "RESPONSABLE HELP-DESK I MICROINFORMÀTICA";
+  document.getElementById("exp-data-3").innerHTML =
+    "Abril/2006 - Setembre/2007";
+  document.getElementById("exp-leyenda-3").innerHTML =
+    "Responsable de l'Help-Desk, Usuaris Locals i Clients en Remot." +
+    "<br>" +
+    "Compres, Integració i configuració Equips Informàtics." +
+    "<br>" +
+    "Gestió d'Incidències entre Clients finals i el dpt. de Desenvolupament." +
+    "<br>" +
+    "Documentació del Software desenvolupat.";
+  document.getElementById("exp-tl-4").innerHTML =
+    "Desenvolupament Aplicacions WEB Front-end Back-end";
+  document.getElementById("exp-data-4").innerHTML = "2019";
+  document.getElementById("exp-tl-5").innerHTML =
+    "Desenvolupament Java, Script Bash Linux";
+  document.getElementById("exp-leyenda-4").innerHTML =
+    "Retoc Gràfic: Photoshop." +
+    "<br>" +
+    "Disseny Web: Bootstrap, WordPress." +
+    "<br>" +
+    "Llenguatges de programació: HMTL5, CSS3, JavaScript, Jquery, PHP, SQL, XML, Java, Android, Mit App Inventor 2." +
+    "<br>" +
+    "Estándares de programación WEB: Estricto seguimiento de las recomendaciones de w3schools." +
+    "<br>" +
+    "Atenció Personalitzada a clients." +
+    "<br>" +
+    "Hosting & Posicionament SEO";
+  document.getElementById("exp-leyenda-5").innerHTML =
+    "Desenvolupament i mantenimineto dels Projectes Java existents" +
+    "<br>" +
+    "Llenguatges de programació: Java, Script Bash" +
+    "<br>" +
+    "Estàndards: Ide Eclipse, SVN, Git";
+  document.getElementById("exp-data-5").innerHTML = "2018";
+  document.getElementById("estudios-1").innerHTML =
+    "Grau Superior Desenvolupament Aplicacions WEB";
+  document.getElementById("estudios-52").innerHTML =
+    "Desenvolupament Aplicacions WEB amb Java, JSP, Servlet, Spring, Struts, Hibernate";
+  document.getElementById("centro-1").innerHTML = "Any realització 2017-2018 ";
+  document.getElementById("formacion").innerHTML = "formació";
+  document.getElementById("estudios-2").innerHTML =
+    "Desenvolupament d´aplicacions JAVA";
+  document.getElementById("centro-2").innerHTML = "Any realització 2018";
+  document.getElementById("centro-7").innerHTML = "Any realització 2017";
+  document.getElementById("estudios-3").innerHTML =
+    "Desenvolupament JAVA, Programació Mòbils Avançada XML";
+  document.getElementById("estudios-4").innerHTML =
+    "FP-II Informàtica de Gestió ";
+  document.getElementById("estudios-5").innerHTML =
+    "Formació continuada Autodidacta desenvolupament";
+  document.getElementById("estudios-20").innerHTML =
+    "Curs Social Media Market Orientat a l'Comerç i Xarxes Socials";
+  document.getElementById("estudios-51").innerHTML =
+    "Mit App Inventor 2" +
+    "<br>" +
+    "Certificat SoloLearn SQL Fundaments course" +
+    "<br>" +
+    "posicionament SEO" +
+    "<br>" +
+    "Angular" +
+    "<br>" +
+    "Python" +
+    "<br>" +
+    "Sass" +
+    "<br>" +
+    "SQL con Oracle 11g" +
+    "<br>" +
+    "C y C++";
+  document.getElementById("estudios-6").innerHTML =
+    "Curs de Desemvolupament Back-end amb PHP";
+  document.getElementById("centro-6").innerHTML = "Any realització 2019";
+  document.getElementById("centro-16").innerHTML = "Any realització 2019";
+  document.getElementById("estudios-71").innerHTML =
+    "Curs SEO-SEM Posicionament WEB";
+  document.getElementById("centro-3").innerHTML = "Any realització 2014-2016 ";
+  document.getElementById("centro-4").innerHTML = "Any realització 1990 ";
+  document.getElementById("centro-8").innerHTML = "Any realització 2020";
+  document.getElementById("Interes-Lot").innerHTML = "COMPETÈNCIA";
+  document.getElementById("compete").src = "img/competencias-cat.jpg";
+  document.getElementById("activa").innerHTML =
+    "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspInformació obtinguda a través dels serveis de competència de";
+
+  /*    document.getElementById('Interes-l1').innerHTML ="Sóc una persona resolutiva. Capaç de portar la iniciativa en els treballs d'equip i en situacions difícils o estrès. Tinc disposició participar i aportar idees. Sóc capaç de preveure problemes i anticipar-me amb la solució.";
   document.getElementById('Interes-l2').innerHTML ="No perdo de vista els objectius marcats. Busco la perfecció en els meus treballs. M'agrada afrontar nous reptes. Sóc perseverant i tenaç amb les noves tasques. Ambiciós i exigent amb mi mateix.";
   document.getElementById('Interes-l3').innerHTML ="Capacitat d'organització i coordinar activitats i equips, establint l'ordre lògic de la seva execució. Planifico a llarg termini, definint plans d'acció basant-me en les prioritats i objectius.";
   document.getElementById('Interes-l4').innerHTML ="Sóc una persona detallista i minuciosa en tot allò que faig. Sóc molt rigorós amb el treball i vel per la qualitat d'aquest. Exigeixo que les normes siguin clares i per escrit.";
@@ -80,5 +212,4 @@ document.getElementById('catala').onclick = function(){
   document.getElementById('Interes-l12').innerHTML ="M'agrada investigar a consciència, dedico part del meu temps lliure a l'adquisició de nous coneixements, sóc molt proactiu en la recerca de coneixements.";
   document.getElementById('Interes-l13').innerHTML ="Analitzo els pros i contres de les decisions.";
 */
-
-}
+};
